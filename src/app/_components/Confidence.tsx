4d806f64e1891f5b30bb3e8c036b62b1cee44f78
Michael Leif <michaeldarmali1683@gmail.com>
2024-12-14 22:44:44 +1100

    added functional colors based on confidence rating

--- src/app/_components/Confidence.tsx
+++ src/app/_components/Confidence.tsx
@@ -1,18 +1,31 @@
+"use client"
+
 import React from 'react'
 import { MdOutlineSentimentVeryDissatisfied } from "react-icons/md";
 import { MdOutlineSentimentDissatisfied } from "react-icons/md";
 import { MdOutlineSentimentNeutral } from "react-icons/md";
 import { MdOutlineSentimentSatisfied } from "react-icons/md";
 import { MdSentimentVerySatisfied } from "react-icons/md";
+import { IconContext } from 'react-icons';
 
 export default function Confidence({ confidence }: { confidence: number }) {
   return (
     <div className="flex gap-2">
-        <MdOutlineSentimentVeryDissatisfied className="text-2xl" />
-        <MdOutlineSentimentDissatisfied className="text-2xl" />
-        <MdOutlineSentimentNeutral className="text-2xl" />
-        <MdOutlineSentimentSatisfied className="text-2xl" />
-        <MdSentimentVerySatisfied className="text-2xl" />
+        <IconContext.Provider value={{ color: (confidence == 1) ? 'red' : 'white' }}>
+            <MdOutlineSentimentVeryDissatisfied className="text-2xl" />
+        </IconContext.Provider>
+        <IconContext.Provider value={{ color: (confidence == 2) ? 'orange' : 'white' }}>
+            <MdOutlineSentimentDissatisfied className="text-2xl" />
+        </IconContext.Provider>
+        <IconContext.Provider value={{ color: (confidence == 3) ? 'yellow' : 'white' }}>
+            <MdOutlineSentimentNeutral className="text-2xl" />
+        </IconContext.Provider>
+        <IconContext.Provider value={{ color: (confidence == 4) ? '#90EE90' : 'white' }}>
+            <MdOutlineSentimentSatisfied className="text-2xl" />
+        </IconContext.Provider>
+        <IconContext.Provider value={{ color: (confidence == 5) ? 'green' : 'white' }}>
+            <MdSentimentVerySatisfied className="text-2xl" />
+        </IconContext.Provider>
     </div>
   )
 }
